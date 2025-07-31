1c58c2ef52b05b811373c6e5adfa2ff2c244f16c
jhue <threeax@foxmail.com>
2025-07-31 16:25:10 +0800

    feat: 适应新版本插件加载修改

--- go.mod
+++ go.mod
@@ -6,11 +6,11 @@ require (
 	github.com/disintegration/imaging v1.6.2
 	github.com/fogleman/gg v1.3.0
 	github.com/kohmebot/gn8 v0.0.1
-	github.com/kohmebot/pkg v1.0.1
+	github.com/kohmebot/pkg v1.0.2
 	github.com/kohmebot/plugin v1.2.5
 	github.com/sirupsen/logrus v1.9.3
 	github.com/stretchr/testify v1.9.0
-	github.com/wdvxdr1123/ZeroBot v1.7.5
+	github.com/wdvxdr1123/ZeroBot v1.8.1
 	gorm.io/gorm v1.25.12
 )
 
--- livebili/conf.go
+++ livebili/conf.go
@@ -6,31 +6,31 @@ import (
 
 type Config struct {
 	// 订阅的uid
-	Uids []int64 `yaml:"uids" mapstructure:"uids"`
+	Uids []int64 `yaml:"uids" `
 	// 指定分群订阅uid
-	GroupUids map[int64][]int64 `yaml:"group_uids" mapstructure:"group_uids"`
+	GroupUids map[int64][]int64 `yaml:"group_uids"`
 	// 字体文件路径
 	TTF string `yaml:"ttf" mapstructure:"ttf"`
 	// 检查直播间状态的间隔时间，单位为秒
-	CheckLiveDuration int `yaml:"check_live_duration" mapstructure:"check_live_duration"`
+	CheckLiveDuration int `yaml:"check_live_duration" `
 	// 直播开始时推送的提示语
-	LiveTips []string `yaml:"live_tips" mapstructure:"live_tips"`
+	LiveTips []string `yaml:"live_tips" `
 	// 直播结束时推送的提示语
-	OffTips []string `yaml:"off_tips" mapstructure:"off_tips"`
+	OffTips []string `yaml:"off_tips" `
 	// 下播是否推送
-	SendOff bool `yaml:"send_off" mapstructure:"send_off"`
+	SendOff bool `yaml:"send_off" `
 	// 检查动态的间隔时间，单位为秒
-	CheckDynamicDuration int `yaml:"check_dynamic_duration" mapstructure:"check_dynamic_duration"`
+	CheckDynamicDuration int `yaml:"check_dynamic_duration" `
 	// 检查粉丝数的间隔时间，单位为秒
-	CheckFollowerDuration int `yaml:"check_follower_duration" mapstructure:"check_follower_duration"`
+	CheckFollowerDuration int `yaml:"check_follower_duration" `
 	// 粉丝数变化时，每多少个推送一次
-	FollowerNotifyEach int `yaml:"follower_notify_each" mapstructure:"follower_notify_each"`
+	FollowerNotifyEach int `yaml:"follower_notify_each"`
 	// 当达到这个时间间隔时，推送一次粉丝数变化
-	FollowerNotifyDuration int `yaml:"follower_notify_duration" mapstructure:"follower_notify_duration"`
+	FollowerNotifyDuration int `yaml:"follower_notify_duration"`
 	// b站的cookies
-	Cookies string `yaml:"cookies" mapstructure:"cookies"`
+	Cookies string `yaml:"cookies"`
 	// 是否at全员
-	AtAll bool `yaml:"at_all" mapstructure:"at_all"`
+	AtAll bool `yaml:"at_all"`
 }
 
 func (c *Config) randChoseLiveTips() string {
